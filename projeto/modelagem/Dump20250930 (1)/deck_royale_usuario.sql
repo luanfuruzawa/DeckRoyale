bbd4b0f7a60f51395627a877978a513295abb65a
luanfuruzawa <20241ctb0100006@estudantes.ifpr.edu.br>
2025-10-04 00:20:23 -0300

    arrumar dumps

--- projeto/modelagem/Dump20250930 (1)/deck_royale_usuario.sql
+++ projeto/modelagem/Dump20250930 (1)/deck_royale_usuario.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: deckroyale
+-- Host: localhost    Database: deck_royale
 -- ------------------------------------------------------
--- Server version	8.4.4
+-- Server version	8.3.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,27 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `deck`
+-- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `deck`;
+DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `deck` (
+CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) DEFAULT NULL,
-  `tipo` varchar(50) DEFAULT NULL,
+  `nome` varchar(20) NOT NULL,
+  `perfil` varchar(50) NOT NULL DEFAULT 'User',
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `deck`
+-- Dumping data for table `usuario`
 --
 
-LOCK TABLES `deck` WRITE;
-/*!40000 ALTER TABLE `deck` DISABLE KEYS */;
-/*!40000 ALTER TABLE `deck` ENABLE KEYS */;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Murilo','User','murilo@gmail.com','1234'),(5,'Luan','Admin','luan@gmail.com','$2y$10$zWR6g7meegMeYPgvO3kjMOaU4eqwYtZ4aG9DbXOTOgHwsyYJrZdY2'),(7,'Matheus','User','marheus@gmail.com','$2y$10$xFOOzMFuGwGFsSppFahu9uJE63GQ4eJzRnSVc06sGX1xmdcXzl/c6');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -48,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-30 12:29:00
+-- Dump completed on 2025-10-04  0:18:15
